e9f750dd1db62ba085a933f60dbfd3475e6192b9
Moim HA <moimhossain@gmail.com>
2025-09-09 07:26:27 +0200

    Add transaction endpoint with telemetry tracking
    Implemented a new POST endpoint for handling transactions in `Program.cs`, including metrics for duration and specific headers for telemetry. Updated the Application Insights connection string with a valid key. Added new POST requests in `DiagnosticsApp.http` and `SigningAPI.http` for testing the transaction endpoint, along with a GET request for a hello world response.

--- DiagnosticsApp/SigningAPI/Program.cs
+++ DiagnosticsApp/SigningAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -7,7 +11,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddApplicationInsightsTelemetry(options =>
 {
     // Hardcoded instrumentation key - replace with actual key in production
-    options.ConnectionString = "";
+    options.ConnectionString = "InstrumentationKey=d6ed2722-7b8c-4350-ba39-e3cdc43e7ebb;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/;ApplicationId=2ce6b7ee-0cea-4cdd-9f1a-25402734997a";
 });
 
 var app = builder.Build();
@@ -19,40 +23,59 @@ app.MapGet("/hello", () => "Hello world!").WithOpenApi();
 
 app.MapPost("/transaction", async (TransactionRequest request, HttpContext context, IHttpClientFactory httpClientFactory, Microsoft.ApplicationInsights.TelemetryClient telemetryClient) =>
 {
+
+    var startTime = DateTime.UtcNow;
+
     var client = httpClientFactory.CreateClient();
-    
+
     var apiUrl = "https://solarapimuat.azure-api.net/casper/transaction";
     var response = await client.PostAsJsonAsync(apiUrl, request);
-    
+
+
+    var endTime = DateTime.UtcNow;
+    var duration = (endTime - startTime).TotalMilliseconds;
+
+    var reponseProperties = new Dictionary<string, object>();
+
     // Headers we want to track
     var headerKeysToTrack = new[]
     {
         "x-apim-tx-duration",
-        "x-apim-tx-endtime", 
+        "x-apim-tx-endtime",
         "x-apim-tx-starttime",
-        "x-casper-tx-duration", 
-        "x-casper-tx-endtime", 
+        "x-casper-tx-duration",
+        "x-casper-tx-endtime",
         "x-casper-tx-starttime"
     };
-    
+
     // Dictionary to store header values we want to track
     var headerMetrics = new Dictionary<string, string>();
-    
+
     // Forward the response headers from Casper API
     foreach (var header in response.Headers)
     {
         if (!context.Response.Headers.ContainsKey(header.Key))
         {
-            context.Response.Headers.Append(header.Key, header.Value.ToArray());
+            reponseProperties.Add(header.Key, header.Value.ToArray());
         }
-        
+
         // Store header values for metrics if they match our list of headers to track
         if (headerKeysToTrack.Contains(header.Key.ToLower()))
         {
             headerMetrics[header.Key.ToLowerInvariant()] = string.Join(",", header.Value);
         }
     }
-    
+
+    reponseProperties.Add("x-signin-startTime", startTime.ToString("o"));
+    reponseProperties.Add("x-signin-endTime", endTime.ToString("o"));
+    reponseProperties.Add("x-signin-duration", duration.ToString());
+
+    // add our own metrics
+    headerMetrics["x-signin-starttime"] = startTime.ToString("o");
+    headerMetrics["x-signin-endtime"] = endTime.ToString("o");
+    headerMetrics["x-signin-duration"] = duration.ToString();
+
+
     // Track headers as metrics
     if (headerMetrics.Any())
     {
@@ -61,40 +84,38 @@ app.MapPost("/transaction", async (TransactionRequest request, HttpContext conte
         {
             ["TransactionId"] = request.TransactionId.ToString()
         };
-        
+
         // Add all headers to properties
         foreach (var header in headerMetrics)
         {
             properties[header.Key] = header.Value;
         }
-        
+
         // Track as event with all properties
         telemetryClient.TrackEvent("ApiHeaderMetrics", properties);
-        
+
         // Track specific numeric values as metrics
-        if (headerMetrics.TryGetValue("x-apim-tx-duration", out var apimDurationStr) && 
+        if (headerMetrics.TryGetValue("x-apim-tx-duration", out var apimDurationStr) &&
             double.TryParse(apimDurationStr, out var apimDuration))
         {
             telemetryClient.TrackMetric("ApimTxDuration", apimDuration);
         }
-        
-        if (headerMetrics.TryGetValue("x-casper-tx-duration", out var casperDurationStr) && 
+
+        if (headerMetrics.TryGetValue("x-casper-tx-duration", out var casperDurationStr) &&
             double.TryParse(casperDurationStr, out var casperDuration))
         {
             telemetryClient.TrackMetric("CasperTxDuration", casperDuration);
         }
+
+        if (headerMetrics.TryGetValue("x-signin-duration", out var signinDurationStr) &&
+            double.TryParse(signinDurationStr, out var signinDuration))
+        {
+            telemetryClient.TrackMetric("SigninDuration", signinDuration);
+        }
     }
-    
-    // Return the response from the external API
-    if (response.IsSuccessStatusCode)
-    {
-        var result = await response.Content.ReadFromJsonAsync<object>();
-        return Results.Ok(result);
-    }
-    else
-    {
-        return Results.StatusCode((int)response.StatusCode);
-    }
+    // return reponseProperties
+    return Results.Ok(new { reponseProperties, request });
+
 }).WithOpenApi();
 
 app.Run();
